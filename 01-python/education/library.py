860a643e6fb4dd7b51588ac75f1faedad59a86b6
Johanne Antoine <jantoine@bu.edu>
2021-09-23 14:48:57 -0400

    optimize code

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,13 +1,23 @@
 def draw_library():
     #roof
-    letter = 'L'
+    letter = '/'
     number = 16
-    for i in range(0, number, 2):
-        print((letter*i).rjust(number-1) + letter + (letter*i).ljust(number-1))
+
+    for i in range(0, number):
+        print((letter).rjust(number-i, ' ') + (' '*i) + ('\\').rjust(i+1))
+    #title
     print('LIBRARY'.center(31,'-'))
+    print('|                             |')
+    print('|                             |')
+    #windows
+    print('|        ----       ----      |')
+    for i in range(3):
+        print('|       |    |     |    |     |')
+    
+    print('|        ----       ----      |')
     for i in range(9):
         print('|                             |')
-
+    #roof
     print('|          -----------        |')
     for i in range(5):
         print('|          |         |        |')
