9c19efc2b14aecd85b9559996feda4dcaa7c59a4
Johanne Antoine <jantoine@bu.edu>
2021-09-23 14:35:28 -0400

    add to function to print library

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,17 @@
 def draw_library():
-    print("library not found")
+    #roof
+    letter = 'L'
+    number = 16
+    for i in range(0, number, 2):
+        print((letter*i).rjust(number-1) + letter + (letter*i).ljust(number-1))
+    print('LIBRARY'.center(31,'-'))
+    for i in range(9):
+        print('|                             |')
+
+    print('|          -----------        |')
+    for i in range(5):
+        print('|          |         |        |')
+    print('-'*31)
     return
+
+draw_library()
