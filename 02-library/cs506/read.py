75fcb11da04d6413bc0afe288776c242be3ee2c4
Johanne Antoine <jantoine@bu.edu>
2021-09-30 15:11:37 -0400

    change file

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,11 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    page = open(csv_file_path)
+    lines = page.readlines()
+    X = []
+    for line in lines:
+        xlist = line.split(',')
+        xlist = [int(x) if (x.strip()).isnumeric() else x.strip("\"") for x in xlist]
+        X.append(xlist)
+    return X
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,10 +5,18 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersect = len(set(x).intersection(set(y)))
+    print('intersect: {i}'.format(i=intersect))
+    union = (len(x) + len(y)) - intersect
+    similarity = intersect/union
+    return 1 - similarity
+    
 
 def cosine_sim(x, y):
     raise NotImplementedError()
